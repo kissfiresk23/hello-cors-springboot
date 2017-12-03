9dee47c0a0ba3bf0529646268bf5e6090a9c374e
yongfengwang <yongfengwang@infinite.net.cn>
2017-12-03 12:28:08 +0800

    加了注释

--- src/main/java/com/wyf/test/hellospringbootmvconly/config/cors/CorsIntercetor.java
+++ src/main/java/com/wyf/test/hellospringbootmvconly/config/cors/CorsIntercetor.java
@@ -12,8 +12,8 @@ public class CorsIntercetor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 该配置已经测试了支持GET、POST、PUT、DELETE、OPTIONS、ContentType是application/json的POST方式
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");// 不明白，为什么要明确指定，用*就会导致PUT、DELETE失效
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type,Origin,Accept");// 同样这里不能用*
+		response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");// 不明白，为什么要明确指定，用*就会导致PUT、DELETE、传JSON失效
+		response.setHeader("Access-Control-Allow-Headers", "*");// 同样这里不能用*，否则会倒着传JSON失效（奇怪的是PUT和DELETE正常）
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Max-Age", "1800");
 		return true;
